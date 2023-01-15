d69942b5a2d639bfb7b4d695f312ce27e5aff3f5
Phillip Webb <pwebb@vmware.com>
2023-01-15 12:13:13 -0800

    Add sanitizer and show env values

--- dog-service/src/main/java/com/example/dogservice/DogServiceApplication.java
+++ dog-service/src/main/java/com/example/dogservice/DogServiceApplication.java
@@ -1,8 +1,11 @@
 package com.example.dogservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.endpoint.SanitizableData;
+import org.springframework.boot.actuate.endpoint.SanitizingFunction;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Main application entry point.
@@ -10,6 +13,13 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @SpringBootApplication(proxyBeanMethods = false)
 @ConfigurationPropertiesScan
 public class DogServiceApplication {
+
+	@Bean
+	SanitizingFunction sanitizingFunction() {
+		return (data) -> (data.getKey().toLowerCase().contains("password"))
+				? data.withValue(SanitizableData.SANITIZED_VALUE)
+				: data;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DogServiceApplication.class, args);
