9743cd4155a94807a6f21e21a780627f66852f4c
Phillip Webb <pwebb@vmware.com>
2023-01-15 16:09:11 -0800

    Fix Spring Data repositories

--- dog-service/src/main/java/com/example/dogservice/domain/DogRepository.java
+++ dog-service/src/main/java/com/example/dogservice/domain/DogRepository.java
@@ -2,12 +2,13 @@ package com.example.dogservice.domain;
 
 import java.util.List;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.ListPagingAndSortingRepository;
 
 /**
  * Spring Data repository for {@link Dog} entities.
  */
-public interface DogRepository extends PagingAndSortingRepository<Dog, Long> {
+public interface DogRepository extends ListCrudRepository<Dog, Long>, ListPagingAndSortingRepository<Dog, Long> {
 
 	List<Dog> findByOwner(Owner owner);
 
--- dog-service/src/main/java/com/example/dogservice/domain/OwnerRepository.java
+++ dog-service/src/main/java/com/example/dogservice/domain/OwnerRepository.java
@@ -1,11 +1,11 @@
 	package com.example.dogservice.domain;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.ListCrudRepository;
 
 /**
  * Spring Data repository for {@link Owner} entities.
  */
-public interface OwnerRepository extends CrudRepository<Owner, Long> {
+public interface OwnerRepository extends ListCrudRepository<Owner, Long> {
 
 	Owner findByNameIgnoringCase(String name);
 
